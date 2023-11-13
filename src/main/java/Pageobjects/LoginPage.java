1aa6cb60cf465fd11810cdfe7c7ec481d60b9bf7
raviraj1995 <140617735+raviraj1995@users.noreply.github.com>
2023-11-14 00:46:31 +0530

    Update LoginPage.java

--- src/main/java/Pageobjects/LoginPage.java
+++ src/main/java/Pageobjects/LoginPage.java
@@ -4,6 +4,10 @@ public class LoginPage {
 	
 	public static void loginPage() {
 		System.out.println("login page");
+		
 	}
-
+	public void login()
+	{
+		System.out.println("another method")
+	}
 }
